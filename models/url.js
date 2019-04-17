e7fab026297d71eb429134febbd8e3de6af6df32
Leonardo Dias <leonardodias914@gmail.com>
2019-04-17 17:38:01 -0300

    add hashString to mongoose object

--- models/url.js
+++ models/url.js
@@ -1,30 +1,17 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var CounterSchema = Schema({
-    _id: {type: String, required: true},
-    seq: { type: Number, default: 0 }
-});
-
-var counter = mongoose.model('counter', CounterSchema);
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 // create a schema for our links
 var urlSchema = new Schema({
-  _id: {type: Number, index: true},
+  hashString: {
+    type: String,
+    index: true
+  },
   long_url: String,
   created_at: Date
-});
-
-urlSchema.pre('save', function(next){
-  var doc = this;
-  counter.findByIdAndUpdate({_id: 'url_count'}, {$inc: {seq: 1} },{'upsert':true, 'new':true}, function(error, counter) {
-      if (error)
-          return next(error);
-      doc.created_at = new Date();
-      doc._id = counter.seq;
-      next();
-  });
-});
+}, { 
+    noVirtualId: true }
+  );
 
 var Url = mongoose.model('Url', urlSchema);
 
