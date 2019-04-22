a25ee3e0ee8a194c36441ab70bd191708812b3b5
Leonardo Dias <leonardodias914@gmail.com>
2019-04-22 10:17:53 -0300

    Change url path

--- public/javascripts/shorten.js
+++ public/javascripts/shorten.js
@@ -1,7 +1,7 @@
 $('.btn-shorten').on('click', function(){
 
   $.ajax({
-    url: '/api/shorten',
+    url: '/shorten',
     type: 'POST',
     dataType: 'JSON',
     data: {url: $('#url-field').val()},
